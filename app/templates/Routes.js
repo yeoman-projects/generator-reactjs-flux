28d1421e12cec37db6b226bbbf0f17084b124992
Jason Johnson <jjohnson@dataminr.com>
2015-04-28 16:32:40 -0600

    Eslint fix.

--- app/templates/Routes.js
+++ app/templates/Routes.js
@@ -2,6 +2,7 @@ define(function(require) {
     'use strict';
 
     var App = require('components/App');
+    var React = require('react');
     var Router = require('react-router');
 
     return <Router.Route name="app" path="/<%= _.slugify(projectName) %>/app/index.html" handler={App} />;
